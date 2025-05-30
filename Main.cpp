c19236a51b24bfab625190adfb04a28642d1f28c
R3sssque <strzesakkacper@gmail.com>
2025-05-30 22:28:29 +0200

    Add model loading

--- Main.cpp
+++ Main.cpp
@@ -1,220 +1,223 @@
-#include <iostream>
-#include <filesystem>
-#include <glad/glad.h>
-#include <GLFW/glfw3.h>
-#include <stb/stb_image.h>
-#include <glm/glm.hpp>
-#include <glm/gtc/matrix_transform.hpp>
-#include <glm/gtc/type_ptr.hpp>
-
-#include "Texture.h"
-#include "shaderClass.h"
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
-#include "Camera.h"
-
+//------- Ignore this ----------
+#include<filesystem>
 namespace fs = std::filesystem;
+//------------------------------
+
+#include"Model.h"
+
 
 const unsigned int width = 800;
 const unsigned int height = 800;
 
-//GLfloat vertices[] =
-//{//	X		Y		Z			R		G		B			Tex0	Tex1
-//	-0.5f,	-0.5f,	-0.5f,		1.0f,	0.0f,	0.0f,		0.0f,	0.0f, // Lower left back
-//	0.5f,	-0.5f,	-0.5f,		0.0f,	1.0f,	0.0f,		1.0f,	0.0f,  // Lower right back
-//	0.5f,	0.5f,	-0.5f,		0.0f,	0.0f,	1.0f,		0.0f,	1.0f,  // Upper right back
-//	-0.5f,  0.5f,	-0.5f,		1.0f,	1.0f,	0.0f,		1.0f,	1.0f, // Upper left back
-//	-0.5f,	-0.5f,  0.5f,		0.0f,	1.0f,	1.0f,		0.0f,	0.0f, // Lower left front
-//	0.5f,	-0.5f,  0.5f,		1.0f,	0.0f,	1.0f,		1.0f,	0.0f,  // Lower right front
-//	0.5f,	0.5f,	0.5f,		1.0f,	1.0f,	1.0f,		0.0f,	1.0f,  // Upper right front
-//	- 0.5f,  0.5f,	0.5f,		0.5f,	0.5f,	0.5f,		1.0f,	1.0f // Upper left front
-//};
-//
-//GLuint indices[] =
-//{
-//	// Back face
-//	0, 1, 2,
-//	2, 3, 0,
-//	// Front face
-//	4, 5, 6,
-//	6, 7, 4,
-//	// Left face
-//	0, 3, 7,
-//	7, 4, 0,
-//	// Right face
-//	1, 5, 6,
-//	6, 2, 1,
-//	// Top face
-//	3, 2, 6,
-//	6, 7, 3,
-//	// Bottom face
-//	0, 1, 5,
-//	5, 4, 0
-//};
 
-GLfloat vertices[] = {
-	// Wierzcho³ki          /  Kolory     /  TexCoord (u, v) //
-	// Przód
-	-0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // 0
-	 0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 1
-	 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // 2
-	-0.5f,  0.5f,  0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // 3
+float rectangleVertices[] =
+{
+	// Coords    // texCoords
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f, -1.0f,  0.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f,
 
-	// Ty³
-	-0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // 4
-	 0.5f, -0.5f, -0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 5
-	 0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // 6
-	-0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // 7
-
-	// Góra
-	-0.5f,  0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // 8
-	 0.5f,  0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 9
-	 0.5f,  0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // 10
-	-0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // 11
-
-	// Dó³
-	-0.5f, -0.5f,  0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // 12
-	 0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 13
-	 0.5f, -0.5f, -0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // 14
-	-0.5f, -0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // 15
-
-	// Lewa
-	-0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,   0.0f, 0.0f,   // 16
-	-0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // 17
-	-0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,   1.0f, 1.0f,   // 18
-	-0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f,   // 19
-
-	// Prawa
-	 0.5f, -0.5f, -0.5f,   1.0f, 0.0f, 0.0f,    0.0f, 0.0f,   // 20
-	 0.5f, -0.5f,  0.5f,   0.0f, 1.0f, 0.0f,    1.0f, 0.0f,   // 21
-	 0.5f,  0.5f,  0.5f,   0.0f, 0.0f, 1.0f,    1.0f, 1.0f,   // 22
-	 0.5f,  0.5f, -0.5f,   1.0f, 1.0f, 0.0f,    0.0f, 1.0f    // 23
+	 1.0f,  1.0f,  1.0f, 1.0f,
+	 1.0f, -1.0f,  1.0f, 0.0f,
+	-1.0f,  1.0f,  0.0f, 1.0f
 };
 
-GLuint indices[] = {
-	0, 1, 2, 2, 3, 0,		// Przód
-	4, 5, 6, 6, 7, 4,		// Ty³
-	8, 9, 10, 10, 11, 8,	// Góra
-	12, 13, 14, 14, 15, 12,	// Dó³
-	16, 17, 18, 18, 19, 16,	// Lewa
-	20, 21, 22, 22, 23, 20	// Prawa
-};
 
 int main()
 {
+	// Initialize GLFW
 	glfwInit();
 
+	// Tell GLFW what version of OpenGL we are using 
+	// In this case we are using OpenGL 3.3
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-
+	// Tell GLFW we are using the CORE profile
+	// So that means we only have the modern functions
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
-	GLFWwindow* window = glfwCreateWindow(width, height, "JakubSputoOpenGL", NULL, NULL);
-
+	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
+	GLFWwindow* window = glfwCreateWindow(width, height, "YoutubeOpenGL", NULL, NULL);
+	// Error check if the window fails to create
 	if (window == NULL)
 	{
 		std::cout << "Failed to create GLFW window" << std::endl;
 		glfwTerminate();
 		return -1;
 	}
-
+	// Introduce the window into the current context
 	glfwMakeContextCurrent(window);
+
+	//Load GLAD so it configures OpenGL
 	gladLoadGL();
+	// Specify the viewport of OpenGL in the Window
+	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
 	glViewport(0, 0, width, height);
 
+
+
+
+
+	// Generates shaders
 	Shader shaderProgram("default.vert", "default.frag");
+	//Shader framebufferProgram("framebuffer.vert", "framebuffer.frag");
 
-	//Texture
-	std::string parentDir = fs::current_path().string();
-	std::string texPath = "/textures/";
+	// Take care of all the light related things
+	glm::vec4 lightColor = glm::vec4(1.0f, 1.0f, 1.0f, 1.0f);
+	glm::vec3 lightPos = glm::vec3(0.5f, 0.5f, 0.5f);
 
-	Texture texWood((parentDir + texPath + "woodTex.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE0, GL_RGBA, GL_UNSIGNED_BYTE);
-	Texture texDirt((parentDir + texPath + "dirtTex.jpg").c_str(), GL_TEXTURE_2D, GL_TEXTURE1, GL_RGBA, GL_UNSIGNED_BYTE);
-	Texture texBrick((parentDir + texPath + "brickTex.png").c_str(), GL_TEXTURE_2D, GL_TEXTURE2, GL_RGBA, GL_UNSIGNED_BYTE);
+	shaderProgram.Activate();
+	glUniform4f(glGetUniformLocation(shaderProgram.ID, "lightColor"), lightColor.x, lightColor.y, lightColor.z, lightColor.w);
+	glUniform3f(glGetUniformLocation(shaderProgram.ID, "lightPos"), lightPos.x, lightPos.y, lightPos.z);
+	//framebufferProgram.Activate();
+	//glUniform1i(glGetUniformLocation(framebufferProgram.ID, "screenTexture"), 0);
 
-	texWood.texUnit(shaderProgram, "tex0", 0);
-	texDirt.texUnit(shaderProgram, "tex0", 1);
-	texBrick.texUnit(shaderProgram, "tex0", 2);
 
-	VAO VAO1;
-	VAO1.Bind();
-
-	VBO VBO1(vertices, sizeof(vertices));
-
-	EBO EBO1(indices, sizeof(indices));
-
-	VAO1.LinkAttrib(VBO1, 0, 3, GL_FLOAT, 8 * sizeof(float), (void*)0);
-	VAO1.LinkAttrib(VBO1, 1, 3, GL_FLOAT, 8 * sizeof(float), (void*)(3 * sizeof(float)));
-	VAO1.LinkAttrib(VBO1, 2, 2, GL_FLOAT, 8 * sizeof(float), (void*)(6 * sizeof(float)));
-
-	VAO1.Unbind();
-	VBO1.Unbind();
-	EBO1.Unbind();
 	
+
+	// Enables the Depth Buffer
 	glEnable(GL_DEPTH_TEST);
 
-	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 3.0f));
+	//// Enables Cull Facing
+	//glEnable(GL_CULL_FACE);
+	//// Keeps front faces
+	//glCullFace(GL_FRONT);
+	//// Uses counter clock-wise standard
+	//glFrontFace(GL_CCW);
 
-	//models
-	//creating models
-	glm::mat4 model1 = glm::mat4(1.0f);
-	glm::mat4 model2 = glm::mat4(1.0f);
-	glm::mat4 model3 = glm::mat4(1.0f);
 
-	//starting positions of models
-	model1 = glm::translate(model1, glm::vec3(-1.5f, 0.0f, 0.0f)); //move -1.5 to the left
-	model2 = glm::translate(model2, glm::vec3(1.5f, 0.0f, 0.0f)); //move 1.5 to the right
-	//model3 = glm::translate(model3, glm::vec3(0.0f, 0.0f, 0.0f)); //stay in the middle
+	// Creates camera object
+	Camera camera(width, height, glm::vec3(0.0f, 0.0f, 2.0f));
 
-	double prevTime = glfwGetTime();
-	float rotation = 1.0f;
 
+	std::string parentDir = (fs::current_path().string());
+	std::string modelPath = "/models/map/scene.gltf";			// DZIA£A
+	//std::string modelPath = "/models/billiards/apucqs.gltf";	// NIE DZIA£A
+	
+	// Load in models
+	Model model((parentDir + modelPath).c_str());
+
+
+	// Prepare framebuffer rectangle VBO and VAO
+	//unsigned int rectVAO, rectVBO;
+	//glGenVertexArrays(1, &rectVAO);
+	//glGenBuffers(1, &rectVBO);
+	//glBindVertexArray(rectVAO);
+	//glBindBuffer(GL_ARRAY_BUFFER, rectVBO);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(rectangleVertices), &rectangleVertices, GL_STATIC_DRAW);
+	//glEnableVertexAttribArray(0);
+	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)0);
+	//glEnableVertexAttribArray(1);
+	//glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
+
+
+
+	// Variables to create periodic event for FPS displaying
+	double prevTime = 0.0;
+	double crntTime = 0.0;
+	double timeDiff;
+	// Keeps track of the amount of frames in timeDiff
+	unsigned int counter = 0;
+
+	// Use this to disable VSync (not advized)
+	//glfwSwapInterval(0);
+
+
+	//// Create Frame Buffer Object
+	//unsigned int FBO;
+	//glGenFramebuffers(1, &FBO);
+	//glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+
+	//// Create Framebuffer Texture
+	//unsigned int framebufferTexture;
+	//glGenTextures(1, &framebufferTexture);
+	//glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+	//glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
+	//glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE); // Prevents edge bleeding
+	//glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, framebufferTexture, 0);
+
+	//// Create Render Buffer Object
+	//unsigned int RBO;
+	//glGenRenderbuffers(1, &RBO);
+	//glBindRenderbuffer(GL_RENDERBUFFER, RBO);
+	//glRenderbufferStorage(GL_RENDERBUFFER, GL_DEPTH24_STENCIL8, width, height);
+	//glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_DEPTH_STENCIL_ATTACHMENT, GL_RENDERBUFFER, RBO);
+
+
+	//// Error checking framebuffer
+	//auto fboStatus = glCheckFramebufferStatus(GL_FRAMEBUFFER);
+	//if (fboStatus != GL_FRAMEBUFFER_COMPLETE)
+	//	std::cout << "Framebuffer error: " << fboStatus << std::endl;
+
+
+	// Main while loop
 	while (!glfwWindowShouldClose(window))
 	{
+		// Updates counter and times
+		crntTime = glfwGetTime();
+		timeDiff = crntTime - prevTime;
+		counter++;
+
+		if (timeDiff >= 1.0 / 30.0)
+		{
+			// Creates new title
+			std::string FPS = std::to_string((1.0 / timeDiff) * counter);
+			std::string ms = std::to_string((timeDiff / counter) * 1000);
+			std::string newTitle = "YoutubeOpenGL - " + FPS + "FPS / " + ms + "ms";
+			glfwSetWindowTitle(window, newTitle.c_str());
+
+			// Resets times and counter
+			prevTime = crntTime;
+			counter = 0;
+
+			// Use this if you have disabled VSync
+			//camera.Inputs(window);
+		}
+
+
+		//// Bind the custom framebuffer
+		//glBindFramebuffer(GL_FRAMEBUFFER, FBO);
+		//// Specify the color of the background
 		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
+		//// Clean the back buffer and depth buffer
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//// Enable depth testing since it's disabled when drawing the framebuffer rectangle
+		//glEnable(GL_DEPTH_TEST);
 
-		shaderProgram.Activate();
-
+		// Handles camera inputs (delete this if you have disabled VSync)
 		camera.Inputs(window);
+		// Updates and exports the camera matrix to the Vertex Shader
+		camera.updateMatrix(45.0f, 0.1f, 100.0f);
 
-		camera.Matrix(45.0f, 0.1f, 100.0f, shaderProgram, "camMatrix");
 
-		//for animations
-		double currentTime = glfwGetTime();
-		if (currentTime - prevTime >= 1.0 / 60)
-			prevTime = currentTime;
+		// Draw the normal model
+		model.Draw(shaderProgram, camera);
 
-		VAO1.Bind();
 
-		texWood.Bind();
-		model1 = glm::rotate(model1, glm::radians(rotation * -1.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMatrix"), 1, GL_FALSE, glm::value_ptr(model1));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
+		//// Bind the default framebuffer
+		//glBindFramebuffer(GL_FRAMEBUFFER, 0);
+		//// Draw the framebuffer rectangle
+		//framebufferProgram.Activate();
+		//glBindVertexArray(rectVAO);
+		//glDisable(GL_DEPTH_TEST); // prevents framebuffer rectangle from being discarded
+		//glBindTexture(GL_TEXTURE_2D, framebufferTexture);
+		//glDrawArrays(GL_TRIANGLES, 0, 6);
 
-		texDirt.Bind();
-		model2 = glm::rotate(model2, glm::radians(rotation), glm::vec3(0.0f, 1.0f, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMatrix"), 1, GL_FALSE, glm::value_ptr(model2));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
 
-		texBrick.Bind();
-		model3 = glm::rotate(model3, glm::radians(rotation), glm::vec3(1.0f, 0.0f, 0.0f));
-		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.ID, "modelMatrix"), 1, GL_FALSE, glm::value_ptr(model3));
-		glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(int), GL_UNSIGNED_INT, 0);
-
+		// Swap the back buffer with the front buffer
 		glfwSwapBuffers(window);
+		// Take care of all GLFW events
 		glfwPollEvents();
 	}
 
 
+
 	// Delete all the objects we've created
-	VAO1.Delete();
-	VBO1.Delete();
-	EBO1.Delete();
-	texWood.Delete();
-	texDirt.Delete();
 	shaderProgram.Delete();
+	//glDeleteFramebuffers(1, &FBO);
 	// Delete window before ending the program
 	glfwDestroyWindow(window);
 	// Terminate GLFW before ending the program
--- Texture.h
+++ Texture.h
@@ -1,16 +1,20 @@
-#pragma once
 #ifndef TEXTURE_CLASS_H
 #define TEXTURE_CLASS_H
+
 #include<glad/glad.h>
 #include<stb/stb_image.h>
+
 #include"shaderClass.h"
+
 class Texture
 {
 public:
 	GLuint ID;
-	GLenum type;
-	Texture(const char* image, GLenum texType, GLenum slot, GLenum format, GLenum
-		pixelType);
+	const char* type;
+	GLuint unit;
+
+	Texture(const char* image, const char* texType, GLuint slot);
+
 	// Assigns a texture unit to a texture
 	void texUnit(Shader& shader, const char* uniform, GLuint unit);
 	// Binds a texture
